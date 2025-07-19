3b7da1634d2b636bf97c986af396e20c9d54caaa
Gevor <gevor@neographgames.com>
2025-07-19 15:36:26 +0400

    Thread test

--- Thread/Thread/Thread.h
+++ Thread/Thread/Thread.h
@@ -18,15 +18,9 @@ int main()
 	std::thread myThread(PrintMessage, "Hello from another thread", 5);
 
 
-	for (int i = 0; i < 5; ++i)
-	{
-		std::cout << "From main: Main thread working [" << i << "]\n";
-	}
 
-
-
-	myThread.join();
-	std::cout << "Main thread finished.\n";
+	bool b = myThread.joinable();
+	std::cout << b << ".\n";
 	return 0;
 }
 
